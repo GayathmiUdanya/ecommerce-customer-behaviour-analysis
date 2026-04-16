0d71210dec01581804479c5e383550d564ee72f7
GayathmiUdanya <gayathmiudanya@gmail.com>
2026-04-16 16:05:29 +0530

    Add SQL queries

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,4 +1,14 @@
-SELECT age, login_frequency
+--Uniques countries 
+SELECT DISTINCT(country)
+FROM ecommerce_customer_behavior;
+
+--The country with the highest average spending 
+SELECT country, AVG(lifetime_value)
 FROM ecommerce_customer_behavior
-WHERE age < 25
-ORDER BY login_frequency ASC;
+GROUP BY country
+ORDER BY AVG(lifetime_value) DESC;
+
+--Average session duration by gender
+SELECT gender, ROUND(AVG(session_duration_avg)::numeric,2)
+FROM ecommerce_customer_behavior
+GROUP BY gender;
